6b2c61ebabe1ed7a7b4c7739b3827cb2c477b002
gurau-anil <gurau.anil@gmail.com>
2025-03-21 00:07:10 -0700

    Dependency resolver project(Project just to resolve dependencies between presentation layer, application layer and infrastructure layer) Infrastructure layer should not be referenced directly in the Presentation layer

---
+++ LibManEase.DependencyInjection/DependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using LibManEase.Application;
+using LibManEase.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibManEase.DependencyResolver
+{
+    public static class DependencyResolver
+    {
+        public static void SetupServiceCollection(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Add Application Services
+            services.AddApplicationServices();
+
+            //Add Infrastructure Services
+            services.AddInfrastructureServices(configuration.GetConnectionString("DefaultConnection")?? String.Empty);
+        }
+    }
+}
