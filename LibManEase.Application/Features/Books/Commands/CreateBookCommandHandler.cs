e2ba2ca3c00868bf9e0da791e82575493cafbfcd
gurau-anil <gurau.anil@gmail.com>
2025-04-14 20:36:33 -0700

    Creating Commands and Queries

---
+++ LibManEase.Application/Features/Books/Commands/CreateBookCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using LibManEase.Application.Abstraction.Features.Books.Commands;
+using LibManEase.Domain.Contracts;
+using LibManEase.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibManEase.Application.Implementation.Features.Books.Commands
+{
+    public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, int>
+    {
+        private readonly IBookRepository _bookRepository;
+        public CreateBookCommandHandler(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<int> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = new Book { };
+            await _bookRepository.AddAsync(book);
+            return book.Id;
+        }
+    }
+}
---
+++ LibManEase.Application/Features/Books/Queries/GetBookByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using LibManEase.Application.Abstraction.DTOs;
+using LibManEase.Application.Abstraction.Features.Books.Queries;
+using LibManEase.Domain.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibManEase.Application.Implementation.Features.Books.Queries
+{
+    public class GetBookByIdHandler : IRequestHandler<GetBookById, BookDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IBookRepository _bookRepository;
+        public GetBookByIdHandler(IMapper mapper, IBookRepository bookRepository)
+        {
+            _mapper = mapper;
+            _bookRepository = bookRepository;
+        }
+        public async Task<BookDto> Handle(GetBookById request, CancellationToken cancellationToken)
+        {
+            return _mapper.Map<BookDto>(await _bookRepository.GetByIdAsync(request.Id));
+        }
+    }
+}
